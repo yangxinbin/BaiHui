3ba6757e079f7f733a9cd4e9af53fc724288ab6a
yangxb <yangxinbin@hotmail.com>
2016-11-25 11:01:03 +0800

    基本主题确认

--- app/src/main/java/com/yxb/baihui/baihui/meipaiimage/windowview/MeipaiimageDetailActivity.java
+++ app/src/main/java/com/yxb/baihui/baihui/meipaiimage/windowview/MeipaiimageDetailActivity.java
@@ -1,8 +1,18 @@
 package com.yxb.baihui.baihui.meipaiimage.windowview;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.yxb.baihui.baihui.R;
+
 /**
  * Created by Administrator on 2016/11/22 0022.
  */
 
-public class MeipaiimageDetailActivity {
+public class MeipaiimageDetailActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+    }
 }
--- app/src/main/java/com/yxb/baihui/baihui/zhihunews/windowview/ZhihunewsDetailActivity.java
+++ app/src/main/java/com/yxb/baihui/baihui/zhihunews/windowview/ZhihunewsDetailActivity.java
@@ -1,8 +1,18 @@
 package com.yxb.baihui.baihui.zhihunews.windowview;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.yxb.baihui.baihui.R;
+
 /**
  * Created by Administrator on 2016/11/22 0022.
  */
 
-public class ZhihunewsDetailActivity {
+public class ZhihunewsDetailActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhihunews_detail);
+    }
 }
