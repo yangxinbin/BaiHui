4df22b8bed80b5450fcb69c18da4035238cb9cda
yangxb <yangxinbin@hotmail.com>
2016-12-03 09:54:26 +0800

    添加新闻URL

--- app/src/main/java/com/yxb/baihui/baihui/urls/Urls.java
+++ app/src/main/java/com/yxb/baihui/baihui/urls/Urls.java
@@ -5,5 +5,24 @@ package com.yxb.baihui.baihui.urls;
  */
 
 public class Urls {
+    //http://v.juhe.cn/toutiao/index?type=top&key=22b714e5c6a52c651a1fd640e47e6383
+    public static final String HOST = "http://v.juhe.cn/toutiao/index";
+    public static final String END_URL = "&key=22b714e5c6a52c651a1fd640e47e6383";
+
+    public static final String TYPE = "?type=";
+
+    public static final String TOP_ID = "top";
+    public static final String SHEHUI_ID = "shehui";
+    public static final String GUOMEI_ID = "guonei";
+    public static final String GUOJI_ID = "guoji";
+    public static final String YULE_ID = "yule";
+    public static final String TIYU_ID = "tiyu";
+    public static final String JUNSHI_ID = "junshi";
+    public static final String KEJI_ID = "keji";
+    public static final String CAIJING_ID = "caijing";
+    public static final String SHISHANG_ID = "shishang";
+
+
+
 
 }
